4d73e005424dc1b4bbda505b822e516b7c1d2e97
Eldar Hasanovic <ehasanovic@indeed.com>
2025-04-15 11:18:25 -0500

    fix(eslint.config.mjs): rewriting eslint config to satisfy eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,44 +2,40 @@ import react from "eslint-plugin-react";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-), {
-    plugins: {
-        react,
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        plugins: {
+            react,
+            "@typescript-eslint": typescriptEslint,
         },
-
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-
-    settings: {
-        react: {
-            version: "detect",
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            ...typescriptEslint.configs.recommended.rules,
+            ...react.configs.recommended.rules,
         },
     },
-
-    rules: {},
-}];
+    {
+        files: ["**/*.test.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+            },
+        },
+    },
+];
